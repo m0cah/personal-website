22ac95d887b7c18d417e85421875314ece370240
Michael Neal <michael.j.neal2003@gmail.com>
2026-01-02 14:08:06 -0500

    Add standlone next configuration to simplify build pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  output: 'standalone',
+  
 }
  
 const withMDX = createMDX({
     extension: /\.(md|mdx)$/,
 })
- 
+
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig)
